1521f8b38a099a7e748a405489975975e32cb77a
tiagobeautiful <tiago.bello1@hotmail.com>
2020-06-07 02:27:17 -0300

    initial commit

---
+++ assets/js/functions.js
@@ -0,0 +1,6 @@
+$('.toogled-nav-wrapper').on('click',function(e){
+  e.stopPropagation();
+});
+$('.nav-toggle-open-wrapper, .nav-toggle-close-wrapper, header .nav-toggle-body-overlay').on('click', function(){
+  $('html').toggleClass('nav-open');
+});
